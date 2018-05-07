4d59ff24db3c1e78523dea0c56b849aa5672d7d6
dkaps125 <dkapit@umd.edu>
2018-05-07 11:39:32 -0400

    Moved into examples folder

--- examples/event_example.go
+++ examples/event_example.go
@@ -2,11 +2,10 @@ package main
 
 import (
     "fmt"
-    "github.com/dkaps125/go-contract"
 )
 
 func main() {
-    var c contract.Contract
+    var c Contract
     c, _ = c.Init("build/contracts/Test.json", "0x345ca3e014aaf5dca488057592ee47305d9b3e10", "http://localhost:9545")
 
     n, _ := c.RegisterEventListener("Event")
--- examples/transact_example.go
+++ examples/transact_example.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "fmt"
-    "github.com/dkaps125/go-contract"
+    "github.com/dkaps125/go-contract/contract"
 )
 
 func main() {
