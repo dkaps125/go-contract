9b4cab98ea13de47e1cd3971e235e3d760c3ea0a
dkaps125 <dkapit@umd.edu>
2018-05-07 11:40:27 -0400

    Fixed event example

--- examples/event_example.go
+++ examples/event_example.go
@@ -2,10 +2,11 @@ package main
 
 import (
     "fmt"
+    "github.com/dkaps125/go-contract/contract
 )
 
 func main() {
-    var c Contract
+    var c contract.Contract
     c, _ = c.Init("build/contracts/Test.json", "0x345ca3e014aaf5dca488057592ee47305d9b3e10", "http://localhost:9545")
 
     n, _ := c.RegisterEventListener("Event")
